9d4a75ca90ad3e8c0af234d2115e9d2db99a0866
Don Schenck <don.schenck@gmail.com>
2017-10-20 15:00:05 -0400

    Changed to use port 8080

--- locationms/Program.cs
+++ locationms/Program.cs
@@ -19,7 +19,7 @@ namespace locationms
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+                .UseStartup<Startup>().UseUrls("http://*:8080")
                 .Build();
     }
 }
