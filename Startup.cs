2aa0434bb5db429de4ef3ccc3b62954d02f89087
Don Schenck <don.schenck@gmail.com>
2019-03-14 13:12:45 -0400

    Added CORS allowance

--- Startup.cs
+++ Startup.cs
@@ -18,11 +18,19 @@ namespace locationms
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => 
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder => {
+                    builder.WithOrigins("*");
+                });
+            });
             services.AddMvc();
         }
 
